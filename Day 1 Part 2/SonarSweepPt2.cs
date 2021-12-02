ebd0b238be033de847753cd80015832c1cb95ab3
sai wentum <michael.wentum@gmail.com>
2021-12-02 06:02:00 -0500

    edit to part 2

--- Day 1 Part 2/SonarSweepPt2.cs
+++ Day 1 Part 2/SonarSweepPt2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace AdventOfCode // Note: actual namespace depends on the project name.
+namespace Day_1_Part_2 // Note: actual namespace depends on the project name.
 {
     public class SonarSweepPt1
     {
@@ -10,10 +10,10 @@ namespace AdventOfCode // Note: actual namespace depends on the project name.
         {
             string[] depths = File.ReadAllLines("D:\\Documents\\random programming stuff\\Advent of code\\2021\\AdventOfCode\\Day 1 Part 2\\test.txt");
 
-            int start = int.Parse(depths[0])+ int.Parse(depths[1])+ int.Parse(depths[2]);
+            int start = int.Parse(depths[0]) + int.Parse(depths[1]) + int.Parse(depths[2]);
             int current = int.Parse(depths[3]) + int.Parse(depths[1]) + int.Parse(depths[2]);
             int count = 0;
-            for (int i = 1; i < depths.Length-3; i++)
+            for (int i = 1; i < depths.Length - 3; i++)
             {
                 if (start < current)
                 {

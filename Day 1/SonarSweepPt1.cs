ceba920e103240e2d6a7463a4fbb99cb13c69021
sai wentum <michael.wentum@gmail.com>
2021-12-01 19:02:07 -0500

    Add project files.

---
+++ Day 1/SonarSweepPt1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode // Note: actual namespace depends on the project name.
+{
+    public class SonarSweepPt1
+    {
+        public static void Main(string[] args)
+        {
+            string[] depth = File.ReadAllLines("D:\\Documents\\random programming stuff\\Advent of code\\2021\\AdventOfCode\\Day 1\\test.txt")
+        }
+    }
+}

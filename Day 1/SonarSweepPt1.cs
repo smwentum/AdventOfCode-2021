3aa4cc1d854bd6877dacffed5d8a95ad629451f2
sai wentum <michael.wentum@gmail.com>
2021-12-01 19:07:10 -0500

    did part 1

--- Day 1/SonarSweepPt1.cs
+++ Day 1/SonarSweepPt1.cs
@@ -8,7 +8,21 @@ namespace AdventOfCode // Note: actual namespace depends on the project name.
     {
         public static void Main(string[] args)
         {
-            string[] depth = File.ReadAllLines("D:\\Documents\\random programming stuff\\Advent of code\\2021\\AdventOfCode\\Day 1\\test.txt")
+            string[] depths = File.ReadAllLines("D:\\Documents\\random programming stuff\\Advent of code\\2021\\AdventOfCode\\Day 1\\test.txt");
+
+            int start = int.Parse(depths[0]);
+            int current; 
+            int count = 0; 
+            for (int i = 1; i < depths.Length;i++)
+            { 
+                current = int.Parse(depths[i]);
+                if (start < current)
+                { 
+                    count++;
+                }
+                start = current;
+            }
+            Console.WriteLine(count);
         }
     }
 }

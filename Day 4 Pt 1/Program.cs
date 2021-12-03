811feb2d05a57c809a92afde7c79e3c143709fea
sai wentum <michael.wentum@gmail.com>
2021-12-03 06:42:26 -0500

    did part 1

---
+++ Day 4 Pt 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"D:\Documents\random programming stuff\Advent of code\2021\AdventOfCode\Day 4 Pt 1\real.txt");
+            string gamma = "";
+            string epsilon = "";
+            int count = 0;
+            for (int i = 0; i < lines[0].Length; i++)
+            {
+                count = 0;
+                for (int j = 0; j < lines.Length; j++)
+                {
+                    if (lines[j][i] == '1')
+                    {
+                        count++;
+                    }
+                }
+                if (count > lines.Length / 2)
+                {
+                    gamma += "1";
+                    epsilon += "0";
+                }
+                else
+                {
+                    gamma += "0";
+                    epsilon += "1";
+                }
+            }
+            //Console.WriteLine(gamma);
+            //Console.WriteLine(epsilon);
+            int answer = Convert.ToInt32(gamma,2)*Convert.ToInt32(epsilon,2);
+            Console.WriteLine(answer);
+        }
+    }
+}

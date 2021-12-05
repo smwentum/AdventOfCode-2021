10b420eb80e29f63524f832dd86af494ef49f875
sai wentum <michael.wentum@gmail.com>
2021-12-05 07:32:38 -0500

    did day 4 part 2

---
+++ Day 4 part 2/Bingo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4_Part_2
+{
+    internal class Bingo
+    {
+        public int square { get; set; }
+        public bool marked { get; set; }
+        public Bingo()
+        {
+
+        }
+
+        public Bingo(int number)
+        {
+            marked = false;
+            square = number;    
+        }
+    }
+}

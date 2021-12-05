cad90e7442467b621192eb9799f3e91abc4032e8
sai wentum <michael.wentum@gmail.com>
2021-12-05 08:44:43 -0500

    did part 2

---
+++ Day 5 Part 2/Line.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_part_2
+{
+    internal class Line
+    {
+        public int x1 { get; set; }
+
+        public int y1 { get; set; }
+
+        public int x2 { get; set; }
+
+        public int y2 { get; set; }
+
+        public int isHorrizontal { get; set; }
+
+        public Line(string LineInformation)
+        {
+            string LineInformationPt2 = LineInformation.Split('-')[0].Trim();
+            x1 = int.Parse(LineInformationPt2.Split(',')[0]);
+            y1 = int.Parse(LineInformationPt2.Split(',')[1]);
+            LineInformationPt2 = LineInformation.Split('>')[1].TrimStart();
+            x2 = int.Parse(LineInformationPt2.Split(',')[0]);
+            y2 = int.Parse(LineInformationPt2.Split(',')[1]);
+
+
+            if (x1 == x2)
+            {
+                isHorrizontal = 1;
+                if (y1 > y2)
+                {
+                    swapPoints();
+                }
+            }
+            else if (y1 == y2)
+            {
+                isHorrizontal = 0;
+                if (x1 > x2)
+                {
+                    swapPoints();
+                }
+            }
+            else if (Math.Abs(x1 - x2) == Math.Abs(y1 - y2))
+            {
+                //this is a diagonal line i think? 
+                isHorrizontal = 2;
+            }
+            else
+            {
+                isHorrizontal = 3;
+            }
+        }
+
+        private void swapPoints()
+        {
+            int x3, y3;
+            x3 = x1;
+            x1 = x2;
+            x2 = x3;
+            y3 = y1;
+            y1 = y2;
+            y2 = y3;
+            
+        }
+    }
+}
---
+++ Day 5 Part 2/Program.cs
@@ -0,0 +1,106 @@
+﻿using Day_5_part_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_5_Part_2 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Line[] lines = File.ReadAllLines(@"D:\Documents\random programming stuff\Advent of code\2021\AdventOfCode\Day 5 part 2\real.txt").Select(m => new Line(m)).ToArray();
+
+            int max = -1;
+            int lineMax = 0;
+            foreach (Line line in lines)
+            {
+                lineMax = Math.Max(Math.Max(line.x1, line.y1), Math.Max(line.x2, line.y2));
+                if (max < lineMax)
+                {
+                    max = lineMax;
+                }
+
+            }
+
+            //now create a grid
+            int[,] grid = new int[max + 2, max + 2];
+
+            //now mark all the ponits 
+            foreach (Line line in lines.Where(m => m.isHorrizontal < 3))
+            {
+                markPointsInGrid(grid, line);
+                //printGrid(grid);
+                //Console.WriteLine();
+            }
+            int count = 0;
+            foreach (int g in grid)
+            {
+                if (g > 1)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        private static void printGrid(int[,] grid)
+        {
+            string line = "";
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    line += grid[i, j].ToString() + " ";
+                }
+                Console.WriteLine(line);
+                line = "";
+            }
+        }
+
+        private static void markPointsInGrid(int[,] grid, Line line)
+        {
+            if (line.isHorrizontal == 0)
+            {
+                for (int i = line.x1; i <= line.x2; i++)
+                {
+                    grid[i, line.y1]++;
+                }
+            }
+
+            else if (line.isHorrizontal == 1)
+            {
+                for (int i = line.y1; i <= line.y2; i++)
+                {
+                    grid[line.x1, i]++;
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= Math.Abs(line.x1 - line.x2); i++)
+                {
+                    if (line.x1 < line.x2 && line.y1 < line.y2)
+                    {
+                        grid[line.x1 + i, line.y1 + i]++;
+                    }
+
+                    else if (line.x1 < line.x2 && line.y1 > line.y2)
+                    {
+                        grid[line.x1 + i, line.y1 - i]++;
+                    }
+
+                    else if (line.x1 > line.x2 && line.y1 < line.y2)
+                    {
+                        grid[line.x1 - i, line.y1 + i]++;
+                    }
+
+                    else
+                    {
+                        grid[line.x1 - i, line.y1 - i]++;
+                    }
+                }
+            }
+        }
+    }
+}

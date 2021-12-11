292b52a4f1addfbf84f47179586ed9b0e212164c
sai wentum <michael.wentum@gmail.com>
2021-12-11 13:49:31 -0500

    did day 10 part 2, realized the test text i copied it was wrong (my bad) 😂😒😒😒😒

--- Day 10 Part 2/Program.cs
+++ Day 10 Part 2/Program.cs
@@ -8,7 +8,7 @@ namespace Day_10_Part_2
     {
         static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines(@"D:\Documents\random programming stuff\Advent of code\2021\AdventOfCode\Day 10 part 2\test.txt");
+            string[] lines = File.ReadAllLines(@"D:\Documents\random programming stuff\Advent of code\2021\AdventOfCode\Day 10 part 2\real.txt");
 
             List<long> answer = new List<long>();
             foreach (string line in lines)
@@ -17,20 +17,54 @@ namespace Day_10_Part_2
                 if(getCorruptedLine(line) == ' ')
                 {
                     Console.WriteLine(getIncompleteLine(line));
-                    //answer.Add(getScore(getIncompleteLine(line))); 
+                    Console.WriteLine(getScore(getIncompleteLine(line)));
+                    answer.Add(getScore(getIncompleteLine(line))); 
 
                 }
             }
-            Console.WriteLine(answer);
+            answer.Sort();
+           Console.WriteLine(answer.Count);
+           Console.WriteLine(answer[answer.Count / 2]);
+        }
+        
+        private static long  getScore(string v)
+        {
+            long answer = 0;
+
+            for (int i = 0; i <v.Length; i++)
+            {
+                answer *= 5;
+                switch (v[i])
+                {
+                    case '(':
+                        answer += 1;
+
+                        break;
+                    case '[':
+                        answer += 2;
+
+                        break;
+                    case '{':
+                        answer += 3;
+
+                        break;
+                    case '<':
+                        answer += 4;
+                        break; 
+                }
+            }
+
+            return answer;
         }
 
         private static string getIncompleteLine(string line)
         {
             Stack<char> stack = new Stack<char>();
+           
 
-            stack.Push(line[0]);
+            //stack.Push(line[0]);
             char c = ' ';
-            for (int i = 1; i < line.Length; i++)
+            for (int i = 0; i < line.Length; i++)
             {
                 switch (line[i])
                 {
@@ -48,19 +82,32 @@ namespace Day_10_Part_2
                         break;
                     case ')':
                         c = stack.Pop();
+                        if (c != '(')
+                        {
+                            Console.WriteLine("something is wrong");
+                        }
                         
                         break;
                     case ']':
                         c = stack.Pop();
-                        
+                        if (c != '[')
+                        {
+                            Console.WriteLine("something is wrong");
+                        }
                         break;
                     case '}':
                         c = stack.Pop();
-                        
+                        if (c != '{')
+                        {
+                            Console.WriteLine("something is wrong");
+                        }
                         break;
                     case '>':
                         c = stack.Pop();
-                       
+                        if (c != '<')
+                        {
+                            Console.WriteLine("something is wrong");
+                        }
                         break;
 
                 }
@@ -70,38 +117,39 @@ namespace Day_10_Part_2
             string answer = "";
             while (stack.Count > 0)
             {
-                switch (stack.Pop())
-                {
-                    case '(':
-                        stack.Push(line[i]);
-                        break;
-                    case '[':
-                        stack.Push(line[i]);
-                        break;
-                    case '{':
-                        stack.Push(line[i]);
-                        break;
-                    case '<':
-                        stack.Push(line[i]);
-                        break;
-                    case ')':
-                        c = stack.Pop();
+                answer += stack.Pop();
+                //switch (stack.Pop())
+                //{ 
+                //    case '(':
+                //        stack.Push(line[i]);
+                //        break;
+                //    case '[':
+                //        stack.Push(line[i]);
+                //        break;
+                //    case '{':
+                //        stack.Push(line[i]);
+                //        break;
+                //    case '<':
+                //        stack.Push(line[i]);
+                //        break;
+                //    //case ')':
+                //    //    c = stack.Pop();
 
-                        break;
-                    case ']':
-                        c = stack.Pop();
+                //    //    break;
+                //    //case ']':
+                //    //    c = stack.Pop();
 
-                        break;
-                    case '}':
-                        c = stack.Pop();
+                //    //    break;
+                //    //case '}':
+                //    //    c = stack.Pop();
 
-                        break;
-                    case '>':
-                        c = stack.Pop();
+                //    //    break;
+                //    //case '>':
+                //    //    c = stack.Pop();
 
-                        break;
+                //    //    break;
 
-                }
+                //}
             }
             return answer; 
         }
